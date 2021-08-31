9b98a96dc4f7517016e142be48e17676755389b9
Vikas Pandey <vikaspa6@gmail.com>
2021-08-31 13:34:23 +0800

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,27 @@
+resource "aws_vpc" "vpc-vikinet" {
+  cidr_block       = "198.168.0.0/22"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "vpc-vikinet"
+  }
+}
+
+resource "aws_subnet" "jumphosts" {
+  vpc_id     = aws_vpc.vpc-vikinet.id
+  cidr_block = "198.168.1.0/24"
+
+  tags = {
+    Name = "snet-jumphosts"
+  }
+}
+
+
+resource "aws_subnet" "snetweb" {
+  vpc_id     = aws_vpc.vpc-vikinet.id
+  cidr_block = "198.168.2.0/24"
+
+  tags = {
+    Name = "snet-web"
+  }
+}
