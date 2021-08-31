4dbc8e85ccbbf143e85bad11790e71fc5e4ab4bd
Vikas Pandey <vikaspa6@gmail.com>
2021-08-31 13:40:06 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,19 @@
+resource "aws_network_interface" "nic-web01" {
+  subnet_id   = aws_subnet.snet-web.id
+  private_ips = ["192.168.1.10"]
+
+  tags = {
+    Name = "nic-pvt-web01"
+  }
+}
+
 resource "aws_instance" "web01" {
   ami           = var.instance_ami
   instance_type = var.instance_type
+  network_interface {
+    network_interface_id = aws_network_interface.nic-web01.id
+    device_index         = 0
+  }
   tags = {
     Name = var.instance_name
   }
